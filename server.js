20c6f35b3d3b9446bd694b0cf3d38293960828f9
aporia-coder <zachary_gray12@yahoo.com>
2020-08-13 06:50:26 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const socket = require("socket.io");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Server connection
+const server = app.listen(port, () => {
+  console.log("Connected to Server");
+});
+
+// Connecting to database
+mongoose.connect(
+  process.env.URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to Database");
+  },
+);
+
+// Socket setup
+const io = socket(server);
+io.on("connection", (socket) => {
+  console.log("Made socket connection");
+});
