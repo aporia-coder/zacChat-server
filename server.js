4958847adb72cf2eda564123a8a556e367171d11
aporia-coder <zachary_gray12@yahoo.com>
2020-09-23 02:58:07 +0100

    save

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-const mongoose = require("mongoose");
 const socket = require("socket.io");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,28 +10,28 @@ const server = app.listen(port, () => {
   console.log(`Connected to Server on port ${port}`);
 });
 
-// Middleware
-// Put static files here when ready to build
-app.use(cors());
-
-// Connecting to database
-mongoose.connect(
-  process.env.URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Connected to Database");
-  },
-);
+app.use(express.static(__dirname + "/public"));
 
 // Socket setup
 const io = socket(server);
 
 io.on("connection", (socket) => {
   console.log("Made socket connection");
+
+  socket.on("add-user", (user) => {
+    io.sockets.emit("add-user", user);
+    console.log(`${user} has connected`);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
+
   socket.on("chat-message", (msg) => {
     io.sockets.emit("chat-message", msg);
   });
 });
+
+// todo
+// emit new users on active user list
+// have to dispatch actions with types ?
